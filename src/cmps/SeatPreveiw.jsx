9bb5ab3c08a8b20eda080cbc7ed733e035246a39
Mor Marzan <mormarzan@gmail.com>
2024-04-29 10:48:54 +0300

    Add hover effect for seat

--- src/cmps/SeatPreveiw.jsx
+++ src/cmps/SeatPreveiw.jsx
@@ -1,13 +1,14 @@
 
 export function SeatPreveiw({ seat, onSelecetSeat, selectedSeatId }) {
-
     const reservedClass = seat.isReserved ? 'reserved' : ''
     const selectedClass = (seat._id === selectedSeatId) ? 'selected' : ''
 
     return (
         <article
             className={`seat-preview ${reservedClass} ${selectedClass}`}
-            onClick={() => onSelecetSeat(seat._id)}>
+            onClick={() => onSelecetSeat(seat._id)}
+        >
+            <span>{seat.loc.col}</span>
         </article>
     )
 }
